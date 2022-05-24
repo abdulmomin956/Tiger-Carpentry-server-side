23c6ae7b591b031e00c3d2c918b0a4caaef18ef4
Abdul <abdulmomin956@gmail.com>
2022-05-24 20:17:24 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.mb5zre9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const productsCollection = client.db("database").collection("product");
+
+        app.post('/products', async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            res.send('good')
+        })
+        app.get('/products', async (req, res) => {
+
+            res.send('good')
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("server is running")
