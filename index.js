73a712f3e94cc9aa17636e5ee83d69a9585cb6bc
Abdul <abdulmomin956@gmail.com>
2022-05-25 11:19:15 +0600

    make admin by patch

--- index.js
+++ index.js
@@ -49,6 +49,18 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
     }
     finally {
 
